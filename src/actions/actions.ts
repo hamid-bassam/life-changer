146f9c0972e4fc3ae44ff978fb09f4aaff842e5b
hamid-bassam <hamid.bassam.147@gmail.com>
2024-08-06 21:27:25 +0200

    actions.ts

---
+++ src/actions/actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import prisma from '@/lib/prisma';
+
+
+export async function createNote() {
+  const note = await prisma.note.create({
+    data: {
+      title: "New Note",
+      content: "New Note Content",
+      userId: 1
+    }
+  });
+  return note;
+}
