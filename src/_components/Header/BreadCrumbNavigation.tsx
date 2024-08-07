eebbaa1add3dc56b02bb472a5c9e4ef0f67ec95e
hamid-bassam <hamid.bassam.147@gmail.com>
2024-08-07 06:58:00 +0200

    bread crumb and navigation links

--- src/_components/Header/BreadCrumbNavigation.tsx
+++ src/_components/Header/BreadCrumbNavigation.tsx
@@ -1,26 +1,56 @@
+"use client"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export const BreadCrumbNavigation = () => {
+  const pathname = usePathname();
+
+  // split pathname by '/'
+  const paths = pathname.split('/').filter(Boolean);
 
   return (
+
     <Breadcrumb className="hidden md:flex">
+
       <BreadcrumbList>
-        <BreadcrumbItem>
+        {/* {paths.map((path, index) => (
+          <>
+          <BreadcrumbItem key={index}>
           <BreadcrumbLink asChild>
-            <Link href="#">Dashboard</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
+                <Link href="#">{path}</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+
+            {index < paths.length - 1 && <BreadcrumbSeparator />}
+
+            </>
+            ))} */}
+
+        {/** iterate over paths except last one */}
+        {paths.slice(0, paths.length - 1).map((path, index) => (
+          <>
+            <BreadcrumbItem key={index}>
+              <BreadcrumbLink asChild>
+                <Link href={`/${paths.slice(0, index + 1).join('/')}`}>
+                  {path.charAt(0).toUpperCase() + path.slice(1)}
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </>
+        ))}
+
+        {/** last path */}
+
         <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="#">Orders</Link>
-          </BreadcrumbLink>
+          <BreadcrumbPage>
+            {paths[paths.length - 1].charAt(0).toUpperCase() + paths[paths.length - 1].slice(1)}
+          </BreadcrumbPage>
         </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Recent Orders</BreadcrumbPage>
-        </BreadcrumbItem>
+
       </BreadcrumbList>
     </Breadcrumb>
+
   );
 };
--- src/_components/NavigationMenu/NotesTooltip.tsx
+++ src/_components/NavigationMenu/NotesTooltip.tsx
@@ -1,20 +1,20 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { PenSquare } from 'lucide-react';
 import Link from 'next/link';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 export const NotesTooltip = () => {
 
   return (
     <Tooltip>
       <TooltipTrigger asChild>
         <Link
-          href="#"
+          href="dashboard/notes"
           className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-primary   md:h-8 md:w-8"
         >
           <PenSquare className="h-5 w-5" />
-          <span className="sr-only">Orders</span>
+          <span className="sr-only">Notes</span>
         </Link>
       </TooltipTrigger>
-      <TooltipContent side="right">Orders</TooltipContent>
+      <TooltipContent side="right">Notes</TooltipContent>
     </Tooltip>
   );
 };
