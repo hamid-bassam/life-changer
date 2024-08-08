1010e1a86e819415c7c8ae9cb94122fd46f471a7
hamid-bassam <hamid.bassam.147@gmail.com>
2024-08-08 04:52:13 +0200

    some goal ui stuff

--- src/_components/GoalCard.tsx
+++ src/_components/GoalCard.tsx
@@ -1,30 +1,136 @@
 "use client";
 // import { PartialBlock } from '@blockGoa/core';
 // import { useCreateBlockGoal } from '@blockGoa/react';
-import { Goal } from '@prisma/client';
-import { Card, CardDescription } from "../components/ui/card";
+import { Goal as GoalType } from '@prisma/client';
 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@radix-ui/react-tooltip';
+import { BellRing, Edit, Ellipsis, Goal, GoalIcon, Paperclip, Plus, SquareCheckBig } from 'lucide-react';
 
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Progress } from '@/components/ui/progress';
+import { Switch } from '@/components/ui/switch';
+
+// import MantineEditor from '../features/blocknote/MantineEditor';
 import { useState } from 'react';
+import { DescriptionScroll } from './description-scroll';
 
 export type GoalCardProps = {
-  goal: Goal
+  goal: GoalType
 };
 
 export const GoalCard = async (props: GoalCardProps) => {
 
   const [isDeleting, setIsDeleting] = useState(false);
+  const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 
   return (
-    <Card
-      className="sm:col-span-2  max-h-64 overflow-y-auto" x-chunk="dashboard-05-chunk-0"
-    >
 
-      <CardDescription className='p-4'> <p>Yes we can </p></CardDescription>
+    <Card className="px-4 py-2 shadow-md rounded-md sm:col-span-2  " x-chunk="dashboard-05-chunk-0">
+      <CardHeader>
+        <CardTitle><span className='flex items-center gap-2'><Goal /> realisation d&apos;app web</span> </CardTitle>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className=" flex items-center space-x-4 rounded-md border p-4">
+          <BellRing />
+          <div className="flex-1 space-y-1">
+            <p className="text-sm font-medium leading-none">
+              Push Notifications
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Send notifications to device.
+            </p>
+          </div>
+          <Switch />
+        </div>
+        <div className=" flex items-center  space-x-4 rounded-md border p-4 ">
 
+          <DescriptionScroll description={description} />
+        </div>
+
+        <div className='inline-flex gap-2'>
+
+          <Badge>Projects</Badge>
+          <Badge variant={'secondary'} className='bg-secondary'>Dev</Badge>
+        </div>
+        <Progress value={1} />
+
+        {/* <div className=' min-w-full bg-transparent nowheel nodrag  ' style={{ overflow: 'auto' }}>
+          <MantineEditor />
+        </div> */}
+      </CardContent>
+      <CardFooter>
+        <div className='flex relative flex-col gap-2 w-full'>
+
+          {/* <Button className="w-full">
+          <Check className="mr-2 h-4 w-4" /> Mark all as read
+        </Button> */}
+          <TooltipProvider>
+            <div className='flex w-full gap-1'>
+
+              <div className='flex-1 mr-auto '></div>
+
+              <Button variant="outline" disabled><Paperclip size={15} /></Button>
+
+              <Tooltip >
+                <TooltipTrigger>
+                  <Button variant="outline"><Edit size={15} /></Button>
+                </TooltipTrigger>
+                <TooltipContent className='bg-popover text-primary'>
+                  <p>Edit</p>
+                </TooltipContent>
+              </Tooltip>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline"><Plus size={15} /></Button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className={`flex  items-center `}>
+
+                  <Tooltip >
+                    <TooltipTrigger>
+                      <DropdownMenuItem className='p-0'>
+                        <Button variant='ghost' className=''>
+                          <GoalIcon size={15} />
+                        </Button>
+                      </DropdownMenuItem>
+                    </TooltipTrigger>
+                    <TooltipContent className='bg-popover text-primary'>
+                      <p>Add a goal</p>
+                    </TooltipContent>
+                  </Tooltip>
+
+                  <Tooltip >
+                    <TooltipTrigger>
+                      <DropdownMenuItem className='p-0'>
+                        <Button variant='ghost' className=''>
+                          <SquareCheckBig size={15} />
+                        </Button>
+                      </DropdownMenuItem>
+                    </TooltipTrigger>
+                    <TooltipContent className='bg-popover text-primary'>
+                      <p>Add a Task</p>
+                    </TooltipContent>
+                  </Tooltip>
+                  <DropdownMenuItem className='p-0 '>
+                    <Button variant='ghost'>
+                      <Ellipsis size={15} />
+                    </Button>
+                  </DropdownMenuItem>
+
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </TooltipProvider>
+        </div>
+      </CardFooter>
 
     </Card>
+
 
   );
 };
---
+++ src/_components/description-scroll.tsx
@@ -0,0 +1,56 @@
+'use client'
+import { ArrowDown, ArrowUp } from 'lucide-react';
+import {
+  useEffect,
+  useRef,
+  useState
+} from 'react';
+import { Button } from '../components/ui/button';
+export type DescriptionScrollProps = {
+  description: string;
+};
+
+export const DescriptionScroll = (props: DescriptionScrollProps) => {
+
+
+  const [fullDescription, setFullDescription] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    if (contentRef.current) {
+      if (fullDescription) {
+        (contentRef.current as HTMLDivElement).style.maxHeight = (contentRef.current as HTMLDivElement).scrollHeight + "px";
+      }
+      else {
+        (contentRef.current as HTMLDivElement).style.maxHeight = "50px";
+      }
+    }
+  }
+    , [fullDescription]);
+
+  return (
+    <span className='flex flex-col justify-center items-center gap-2'>
+      <div
+        ref={contentRef}
+        className={`text-xs overflow-hidden transition-max-height duration-500 ease-out`}
+        style={{ maxHeight: '50px' }}
+      >
+
+        {props.description}
+      </div>
+      {props.description.length > 100 &&
+        <Button className='p-1 h-full m-0 rounded-full bg-primary/30' onClick={async () => {
+          setFullDescription(!fullDescription)
+        }}>
+          {
+            !fullDescription ?
+              <ArrowDown size={10} /> : <ArrowUp size={10} />
+          }
+
+        </Button>
+      }
+    </span>
+  );
+};
+
